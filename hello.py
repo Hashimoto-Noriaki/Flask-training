3d482dc534f0c5f58488dabd61c33f79dfef4004
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-06-19 01:37:39 +0900

    可変のURL2

--- hello.py
+++ hello.py
@@ -2,18 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/japan/tokyo")
-def tokyo():
-    return "Hello,tokyo in Japan"
+@app.route("/japan/<city>")
+def tokyo(city):
+    return f'Hello, { city } in Japan'
 
-@app.route("/japan/ohsaka")
-def ohsaka():
-    return "Hello,ohsaka in Japan"
+# @app.route("/japan/tokyo")
+# def tokyo():
+#     return "Hello,tokyo in Japan"
 
-@app.route("/japan/kyoto")
-def kyoto():
-    return "Hello,kyoto in Japan"
+# @app.route("/japan/ohsaka")
+# def ohsaka():
+#     return "Hello,ohsaka in Japan"
 
+# @app.route("/japan/kyoto")
+# def kyoto():
+#     return "Hello,kyoto in Japan"
 
 
 # @app.route("/baseball/ohtani")
